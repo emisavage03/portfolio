958ca5d04fb22ffe640266f9e06a72451b9dd14b
emisavage03 <emisavage03@gmail.com>
2025-11-11 20:03:53 -0800

    load fix

--- jquery.js
+++ jquery.js
@@ -2,41 +2,41 @@ jQuery(document).ready(function($) {
 
         $('#heavytrafficimg').hide();
         $('#heavytraffic').hover(function() {
-            $('#heavytrafficimg').fadeToggle(200);
+            $('#heavytrafficimg').removeClass("invisible").fadeToggle(200);
     });
 
         $('#springfestgif').hide();
         $('#springfest').hover(function() {
-            $('#springfestgif').fadeToggle(200);
+            $('#springfestgif').removeClass("invisible").fadeToggle(200);
     });
 
         $('#autvisgif').hide();
         $('#autvis').hover(function() {
-            $('#autvisgif').fadeToggle(200);
+            $('#autvisgif').removeClass("invisible").fadeToggle(200);
     });
 
         $('#fuguegif').hide();
         $('#fugue').hover(function() {
-            $('#fuguegif').fadeToggle(200);
+            $('#fuguegif').removeClass("invisible").fadeToggle(200);
     });
 
         $('#fishmusicimg').hide();
         $('#fishmusic').hover(function() {
-            $('#fishmusicimg').fadeToggle(200);
+            $('#fishmusicimg').removeClass("invisible").fadeToggle(200);
     });
 
         $('#sheinfluentialimg').hide();
         $('#sheinfluential').hover(function() {
-            $('#sheinfluentialimg').fadeToggle(200);
+            $('#sheinfluentialimg').removeClass("invisible").fadeToggle(200);
     });
 
         $('#sheddingimg').hide();
         $('#shedding').hover(function() {
-            $('#sheddingimg').fadeToggle(100);
+            $('#sheddingimg').removeClass("invisible").fadeToggle(100);
     });
 
         $('#pinterestartgif').hide();
         $('#pinterestart').hover(function() {
-            $('#pinterestartgif').fadeToggle(100);
+            $('#pinterestartgif').removeClass("invisible").fadeToggle(100);
     });
         });
