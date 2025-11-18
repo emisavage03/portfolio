051d941e2697e7d486e8cd0454d863fc43d0b0ec
emisavage03 <emisavage03@gmail.com>
2025-11-17 23:27:05 -0800

    fixing redundancy

--- jquery.js
+++ jquery.js
@@ -3,60 +3,50 @@ jQuery(document).ready(function($) {
         $('#heavytrafficimg').hide();
         $('.heavytrafficrow').hover(function() {
             $('#heavytrafficimg').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
     });
 
         $('#mmcgif').hide();
         $('.mymotherschildrow').hover(function() {
             $('#mmcgif').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
     });
 
         $('#springfestgif').hide();
         $('.springfestrow').hover(function() {
             $('#springfestgif').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
     });
 
         $('#diszapsterimg').hide();
         $('.diszapsterrow').hover(function () {
             $('#diszapsterimg').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
      });
 
         $('#autvisgif').hide();
         $('.autvisrow').hover(function() {
             $('#autvisgif').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
     });
 
         $('#fuguegif').hide();
         $('.fuguerow').hover(function() {
             $('#fuguegif').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
     });
 
         $('#fishmusicimg').hide();
         $('.fishmusicrow').hover(function() {
             $('#fishmusicimg').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
     });
 
         $('#sheinfluentialimg').hide();
         $('.sheinfluentialrow').hover(function() {
             $('#sheinfluentialimg').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
     });
 
         $('#sheddingimg').hide();
         $('.sheddingrow').hover(function() {
             $('#sheddingimg').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
     });
 
         $('#pinterestartgif').hide();
         $('.pinterestrow').hover(function() {
             $('#pinterestartgif').removeClass("invisible").toggle();
-            $(this).toggleClass("green");
     });
         });
