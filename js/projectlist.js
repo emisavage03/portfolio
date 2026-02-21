8d043939a2d3c775ab6536dc136995026689ae3e
emisavage03 <emisavage03@gmail.com>
2026-02-21 12:36:11 -0800

    project selection, styling, organization

---
+++ js/projectlist.js
@@ -0,0 +1,87 @@
+document.getElementById("projectthin").innerHTML = `
+  <h1 id="index">projects</h1>
+    <hr>
+    <div class="projectlist">
+        <div class="zaprow"> <a class="row" href="zap.html">
+        <p class="left"><mark>ZAP e-board (2025-ongoing)</mark></p>
+        <p class="right"><mark>graphic design and other duties for USC's media arts RSO</mark></p>
+        </a>
+        <hr>
+    </div>
+        <div class="strudelwsrow"> <a class="row" href="strudelworkshop.html">
+        <p class="left"><mark>strudel workshop (2026)</mark></p>
+        <p class="right"><mark>creative club event to teach algorithmic music live coding</mark></p>
+        </a>
+        <hr>
+    </div>
+        <div class="lampshowrow"> <a class="row">
+        <p class="left"><mark>lamp show doc (2025)</mark></p> 
+        <p class="right"><mark>documentary showcasing the AUTOBODY autobody Lamp Show</mark></p>
+        </a>
+        <hr>
+    </div>
+        <div class="heavytrafficrow"> <a class="row" href="project/heavytraffic.html">
+        <p class="left"><mark>heavy traffic (2025)</mark></p>
+        <p class="right"><mark>live sound performance driven by LA traffic cam data</mark></p>
+        </a>
+        <hr>
+        </div>
+        <div class="mymotherschildrow"> <a class="row" href="mymotherschild.html">
+        <p class="left"><mark>my mother's child (2025)</mark></p>
+        <p class="right"><mark>a blanket as a vision of motherhood, daughterhood, and myself in 2035</mark></p>
+        </a>
+        <hr>
+        </div>
+        <div class="springfestrow"> <a class="row" href="project/springfest.html">
+        <p class="left"><mark>USC springfest visualizer (2025)</mark></p>
+        <p class="right"><mark>concert visuals for USC Concerts with Don Toliver and Lyn Lapid</mark></p>
+        </a>
+        <hr>
+        </div>
+        <div class="diszapsterrow"> <a class="row" href="diszapster.html">
+        <p class="left"><mark>diszapster (2025)</mark></p>
+        <p class="right"><mark>a card game played with RGB color and arithmetic</mark></p>
+        </a>
+        <hr>
+        </div>
+        <div class="autvisrow"> <a class="row" href="project/autechrevisualizer.html">
+        <p class="left"><mark>rae by autechre visualizer (2025)</mark></p>
+        <p class="right"><mark>audioreactive visuals made in processing and touchdesigner</mark></p>
+        </a>
+        <hr>
+        </div>
+        <div class="fuguerow"> <a class="row" href="project/fugue.html">
+        <p class="left"><mark>fugue for a specimen (2024)</mark></p>
+        <p class="right"><mark>website about the mirrored medical abuse of two 20th century Black women</mark></p>
+        </a>
+        <hr>
+        </div>
+        <div class="fishmusicrow"> <a class="row" href="project/fishmusic.html">
+        <p class="left"><mark>fish music + other p5 sketches (2024)</mark></p>
+        <p class="right"><mark>fun things i made while learning p5.js</mark></p>
+        </a>
+        <hr>
+        </div>
+        <div class="sheinfluentialrow"> <a class="row" href="project/sheinfluential.html">
+        <p class="left"><mark>sheinfluential (2024)</mark></p>
+        <p class="right"><mark>video essay about the fast fashion industry, glamour, and consumption</mark></p>
+        </a>
+        <hr>
+        </div>
+        <div class="sheddingrow"> <a class="row" href="project/shedding.html">
+        <p class="left"><mark>shedding (2023)</mark></p>
+        <p class="right"><mark>story for a short film about a boy, his uncle, and the things they lost in the shed</mark></p>
+        </a>
+        <hr>
+        </div>
+        <div class="pinterestrow"> <a class="row" href="project/pinfluencer.html">
+        <p class="left"><mark>pinterest art account (2021-2023)</mark></p>
+        <p class="right"><mark>my temporary stint as an art influencer</mark></p>
+        </a>
+        <hr> 
+        </div>
+    </div>
+        
+    </div>
+
+`;
