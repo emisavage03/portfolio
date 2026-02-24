c71113350b2f1a9c172d44b1a7dd4694eebd23e8
emisavage03 <emisavage03@gmail.com>
2026-02-23 17:19:30 -0800

    page fix?

--- js/projectlist.js
+++ js/projectlist.js
@@ -2,67 +2,67 @@ document.getElementById("projectscroll").innerHTML = `
   <h1 id="index">projects</h1>
     <hr>
     <div class="projectlist">
-        <div id="zaprow"> <a class="row" href="/projects/zap.html">
+        <div id="zaprow"> <a class="row" href="projects/zap.html">
         <p class="left"><mark>ZAP marketing (2025-ongoing)</mark></p>
         <p class="right"><mark>graphic design and other duties for USC's media arts RSO</mark></p>
         </a>
         <hr>
     </div>
-        <div id="strudelwsrow"> <a class="row" href="/projects/strudel.html">
+        <div id="strudelwsrow"> <a class="row" href="projects/strudel.html">
         <p class="left"><mark>Strudel workshop (2026)</mark></p>
         <p class="right"><mark>creative club event to teach algorithmic music live coding</mark></p>
         </a>
         <hr>
     </div>
-        <div id="lampshowrow"> <a class="row" href="/projects/lampshow.html">
+        <div id="lampshowrow"> <a class="row" href="projects/lampshow.html">
         <p class="left"><mark>Lamp Show doc (2025)</mark></p> 
         <p class="right"><mark>documentary showcasing the AUTOBODY autobody Lamp Show</mark></p>
         </a>
         <hr>
     </div>
-        <div id="heavytrafficrow"> <a class="row" href="/projects/heavytraffic.html">
+        <div id="heavytrafficrow"> <a class="row" href="projects/heavytraffic.html">
         <p class="left"><mark>Heavy Traffic (2025)</mark></p>
         <p class="right"><mark>code and sound driven by live LA traffic cam data</mark></p>
         </a>
         <hr>
         </div>
-        <div id="mymotherschildrow"> <a class="row" href="/projects/mymotherschild.html">
+        <div id="mymotherschildrow"> <a class="row" href="projects/mymotherschild.html">
         <p class="left"><mark>My Mother's Child (2025)</mark></p>
         <p class="right"><mark>a blanket as a vision of motherhood, daughterhood, and myself in 2035</mark></p>
         </a>
         <hr>
         </div>
-        <div id="springfestrow"> <a class="row" href="/projects/springfest.html">
+        <div id="springfestrow"> <a class="row" href="projects/springfest.html">
         <p class="left"><mark>USC Springfest visualizer (2025)</mark></p>
         <p class="right"><mark>concert visuals for USC Concerts with Don Toliver and Lyn Lapid</mark></p>
         </a>
         <hr>
         </div>
-        <div id="diszapsterrow"> <a class="row" href="/projects/diszapster.html">
+        <div id="diszapsterrow"> <a class="row" href="projects/diszapster.html">
         <p class="left"><mark>DisZAPster (2025)</mark></p>
         <p class="right"><mark>a card game played with RGB color and arithmetic</mark></p>
         </a>
         <hr>
         </div>
-        <div id="fuguerow"> <a class="row" href="/projects/fugue.html">
+        <div id="fuguerow"> <a class="row" href="projects/fugue.html">
         <p class="left"><mark>Fugue for a Specimen (2024)</mark></p>
         <p class="right"><mark>website about the mirrored medical abuse of two 20th century Black women</mark></p>
         </a>
         <hr>
         </div>
-        <div id="fishmusicrow"> <a class="row" href="/projects/fishmusic.html">
+        <div id="fishmusicrow"> <a class="row" href="projects/fishmusic.html">
         <p class="left"><mark>Fish Music + other p5 sketches (2024)</mark></p>
         <p class="right"><mark>fun things i made while learning p5.js</mark></p>
         </a>
         <hr>
         </div>
-        <div id="sheddingrow"> <a class="row" href="/projects/shedding.html">
+        <div id="sheddingrow"> <a class="row" href="projects/shedding.html">
         <p class="left"><mark>Shedding (2023)</mark></p>
         <p class="right"><mark>story for a short film about a boy, his uncle, and the things they lost in the shed</mark></p>
         </a>
         <hr>
         </div>
-        <div id="pinterestrow"> <a class="row" href="/projects/pinterest.html">
+        <div id="pinterestrow"> <a class="row" href="projects/pinterest.html">
         <p class="left"><mark>Pinterest art account (2021-2023)</mark></p>
         <p class="right"><mark>my temporary stint as an art influencer</mark></p>
         </a>
